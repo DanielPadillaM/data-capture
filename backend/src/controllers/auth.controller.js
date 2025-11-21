921749a72408e421592f60cf5a4ad611d29deaa3
Daniel Padilla Moreno <da_2909@hotmail.com>
2025-11-21 08:08:32 -0600

    changes in cookie config

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,6 +5,14 @@ import jwt from 'jsonwebtoken'
 import { createAccessToken } from '../libs/jwt.js'
 import {  NODE_ENV, TOKEN_SECRET } from '../config.js'
 
+const cookieOptions = {
+    httpOnly: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+     path: '/',
+     domain: process.env.FRONTEND_URL
+  };
+
 export const register = async(req,res)=> {
     const {username, email, password}= req.body
 
@@ -25,12 +33,7 @@ export const register = async(req,res)=> {
    const userSaved = await newUser.save()
                 
     const token = await createAccessToken({id:userSaved._id})
-    res.cookie('token',token,{
-        httpOnly: NODE_ENV === "production",
-        secure: NODE_ENV === "production",
-        sameSite: NODE_ENV === "production" ? "none" : "lax"
-
-    })
+    res.cookie('token',token,cookieOptions)
     return res.json({
             id: userSaved._id,
             username: userSaved.username,
@@ -58,14 +61,7 @@ export const login = async(req,res)=> {
         const token = await createAccessToken({id:userFound._id})
         console.log('el token es',token)
         console.log('node',NODE_ENV)
-        res.cookie('token',token,
-           {
-        httpOnly: NODE_ENV === "production",
-        secure: NODE_ENV === "production",
-        sameSite: NODE_ENV === "production" ? "none" : "lax"
-
-    }
-        )
+        res.cookie('token',token,cookieOptions)
         return res.json({
                 id: userFound._id,
                 username: userFound.username,
@@ -78,7 +74,8 @@ export const login = async(req,res)=> {
 }
 
 export const logout = (req,res)=>{
-    res.clearCookie('token')
+     
+    res.clearCookie('token',cookieOptions)
     return res.json({message:"logout"})
 
 }
