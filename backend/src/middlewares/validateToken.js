eda6ad01188839c9e790913253fb34cbc4340e4b
Daniel Padilla Moreno <da_2909@hotmail.com>
2025-11-20 20:11:23 -0600

    problems token

--- backend/src/middlewares/validateToken.js
+++ backend/src/middlewares/validateToken.js
@@ -1,13 +1,18 @@
 import jwt from 'jsonwebtoken'
 import { TOKEN_SECRET } from '../config.js'
 
-export const validateToken = (req,res,next) =>{
+export const validateToken = async (req,res,next) =>{
 
-    const {token} = req.cookies
+    try {
+         const {token} = req.cookies
     if(!token) res.status(401).json({message:"Unauthorized"})
-    jwt.verify(token,TOKEN_SECRET,(err,user) => {
+    await jwt.verify(token,TOKEN_SECRET,(err,user) => {
     if(err) res.status(403).json({message: "invalid token"})
         req.user = user
     })
     next();
+    } catch (error) {
+        console.log(error)
+    }
+   
 }
